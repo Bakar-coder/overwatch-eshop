bc503085e60587df925d3f6971bb5ee75d4aa51f
wabomba-bakar <wabombabakar@gmail.com>
2020-03-09 11:33:28 +0300

    created get cart and post cart controllers.

--- controllers/shop/cart.js
+++ controllers/shop/cart.js
@@ -1,3 +1,24 @@
+const { Product } = require('../../models/Product');
+
 exports.getCart = async (req, res) => {
-  console.log('get cart..............');
+  const cart = await req.user.getCart();
+  const cartProducts = await cart.getProducts();
+  res.json({ success: true, cartProducts });
+};
+
+exports.postCart = async (req, res) => {
+  const { id } = req.body.product;
+  const cart = await req.user.getCart();
+  const cartProduct = await cart.getProducts({ where: { id } });
+  let product;
+  let newQuantity = 1;
+  if (cartProduct.length > 0) {
+    product = cartProduct[0];
+  }
+  if (product) {
+  }
+
+  product = await Product.findById(id);
+  await cart.addProduct(product, { through: { quantity: newQuantity } });
+  res.json({ success: true, cart });
 };
--- routes/api/shop/cart.js
+++ routes/api/shop/cart.js
@@ -1,6 +1,9 @@
+const passport = require('passport');
 const Router = require('express').Router();
-const { getCart } = require('../../../controllers/shop/cart');
+const { getCart, postCart } = require('../../../controllers/shop/cart');
 
-Router.route('/cart').get(getCart);
+Router.route('/')
+  .get(passport.authenticate('jwt', { session: false }), getCart)
+  .post(passport.authenticate('jwt', { session: false }), postCart);
 
 module.exports = Router;
